6fec331952134b742bc3b7167c330c4fe032f2f0
tbng84 <tbng84@gmail.com>
2024-10-10 15:22:56 +0700

    aes encryption

--- ecdh_key_exchange.py
+++ ecdh_key_exchange.py
@@ -1,5 +1,8 @@
 from py_ecc.secp256k1 import secp256k1
 from secrets import randbelow
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+import hashlib
 
 # Generate private keys for party A and B
 private_key_a = randbelow(secp256k1.N)
@@ -17,3 +20,24 @@ shared_secret_b = secp256k1.multiply(public_key_a, private_key_b)
 assert shared_secret_a == shared_secret_b
 
 print("Shared secret:", shared_secret_a)
+
+# Generate an arbitrary message
+message = b"This is a secret message."
+
+# Derive a 256-bit AES key from the shared secret using SHA-256
+shared_secret_bytes = bytes(shared_secret_a[0]) + bytes(shared_secret_a[1])
+aes_key = hashlib.sha256(shared_secret_bytes).digest()
+
+# Encrypt the message using AES
+cipher = AES.new(aes_key, AES.MODE_CBC)
+iv = cipher.iv
+ciphertext = cipher.encrypt(pad(message, AES.block_size))
+
+print("Ciphertext:", ciphertext)
+print("IV:", iv)
+
+# Decrypt the message to verify
+cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+decrypted_message = unpad(cipher.decrypt(ciphertext), AES.block_size)
+
+print("Decrypted message:", decrypted_message)
