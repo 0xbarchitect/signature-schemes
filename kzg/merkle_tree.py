1b7fee410eeeabb50fcef0cb8bbf853cd44cf86a
barchitect0 <tbng84@gmail.com>
2024-10-24 09:12:40 +0700

    merkle tree fix

--- kzg/merkle_tree.py
+++ kzg/merkle_tree.py
@@ -32,7 +32,7 @@ class MerkleTree:
     return proof
 
   def verify_proof(self, leaf, proof, root):
-  #   computed_hash = self._hash(leaf)
+    computed_hash = self._hash(leaf)
   #   for sibling in proof:
   #     if computed_hash < sibling:
   #       computed_hash = self._hash(computed_hash + sibling)
